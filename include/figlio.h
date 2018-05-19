7d8dce90cfe56589d8c6f956e4f964779aa41050
nxttt <39373325+nxttt@users.noreply.github.com>
2018-05-19 17:37:13 +0200

    Added figlio.h

--- include/figlio.h
+++ include/figlio.h
@@ -2,10 +2,10 @@
 #define FIGLIO_H
 
 //wrapper del processo figlio
-void figlio();
+void figlio(int s1Size);
 
 //signal handler
-void status_updated();
+void status_updated(int currentSignal);
 
 //deposita il messaggio di terminazione nella coda di messaggi del processo logger
 void send_terminate();
